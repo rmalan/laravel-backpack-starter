dbc4f8a5c8e91fde813b32de3d7d1f1a818ddaab
M. Raka Maulana A <rmalan09@outlook.com>
2020-04-21 10:24:12 +0700

    Penambahan CRUD example

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,6 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+@role('administrator')
 <!-- Users, Roles, Permissions -->
 <li class="nav-item nav-dropdown">
 	<a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon fa fa-group"></i> Authentication</a>
@@ -9,3 +10,5 @@
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon fa fa-key"></i> <span>Permissions</span></a></li>
 	</ul>
 </li>
+@endrole
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('pegawai') }}'><i class='nav-icon fa fa-question'></i> Pegawai</a></li>
